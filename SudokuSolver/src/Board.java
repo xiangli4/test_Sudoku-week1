dc0fc3a31fc4e6350a3a9f4a38a991623ba6f0f3
xiangli4 <xiang.li4@go.shoreline.edu>
2019-10-04 10:49:17 -0700

    main

--- SudokuSolver/src/Board.java
+++ SudokuSolver/src/Board.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class Board {
     private char[][] arr;
+
     public Board(String fileName) throws FileNotFoundException {
         arr = new char[9][9];
         readFile(fileName);
 
 
     }
+
     public void readFile(String fileName) throws FileNotFoundException {
         File f = new File(fileName);
         Scanner input = new Scanner(f);
         int rowNum = 0;
-        while(input.hasNext()) {
+        while (input.hasNext()) {
             String line = input.next();
             for (int i = 0; i < arr[rowNum].length; i++) {
                 char token = line.charAt(i);
@@ -24,6 +26,7 @@ public class Board {
 
         }
     }
+
     public String toString() {
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < arr.length; i++) {
--- SudokuSolver/src/Main.java
+++ SudokuSolver/src/Main.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        String fileName = "C:\\Users\\happy\\IdeaProjects\\SudokuSolver\\src\\ReadMe.txt";
+        String fileName = "src/testSudoku.sdkx";
         Board b = new Board(fileName);
         System.out.println(b);
 
