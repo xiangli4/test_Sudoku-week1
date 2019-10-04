4e8d71a21770f8e22f742e75fb99f341c7ae991d
xiangli4 <xiang.li4@go.shoreline.edu>
2019-10-03 17:38:23 -0700

    board

--- SudokuSolver/src/Board.java
+++ SudokuSolver/src/Board.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Board {
+    private char[][] arr;
+    public Board(String fileName) throws FileNotFoundException {
+        arr = new char[9][9];
+        readFile(fileName);
+
+
+    }
+    public void readFile(String fileName) throws FileNotFoundException {
+        File f = new File(fileName);
+        Scanner input = new Scanner(f);
+        int rowNum = 0;
+        while(input.hasNext()) {
+            String line = input.next();
+            for (int i = 0; i < arr[rowNum].length; i++) {
+                char token = line.charAt(i);
+                arr[rowNum][i] = token;
+            }
+            rowNum++;
+
+        }
+    }
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                s.append(arr[i][j]);
+            }
+            s.append("\n");
+
+        }
+        return s.toString();
+    }
 }
--- SudokuSolver/src/Main.java
+++ SudokuSolver/src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.io.FileNotFoundException;
 
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileName = "C:\\Users\\happy\\IdeaProjects\\SudokuSolver\\src\\ReadMe.txt";
+        Board b = new Board(fileName);
+        System.out.println(b);
+
+    }
 }
